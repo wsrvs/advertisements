c1f0925dfcd2752ab85ad26f2c0e7c40f14f5826
shirokov <vic.rt@mail.ru>
2023-10-17 22:40:01 +0400

    2023.10.17
    2023.10.17

---
+++ app_advertisements/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
+
+
+# Create your views here.
