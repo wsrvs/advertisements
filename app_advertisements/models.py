cb6b3653f3c4e3991219f9976eac176a2de1be3f
wsrvs <122815322+wsrvs@users.noreply.github.com>
2023-10-18 20:57:39 +0400

    Add files via upload

--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Advertisiment(models.Model):
+    title = models.CharField('Заголок', max_length=128)
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    auctions = models.BooleanField('Торг', help_text='Укажите, уместен ли торг')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'advertisements'
